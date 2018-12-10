2c5fa2bb9d20eb1a671e1152178467fdfeeb8b8a
Mario <45042384+mariosanca@users.noreply.github.com>
2018-12-10 06:34:08 +0100

    Ejercicio acabado

---
+++ EjerciciosUnity/Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    public Animator anim;
+    public bool pausa;
+
+	// Use this for initialization
+	/*void Start () {
+
+        pausa = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            anim.SetBool("activar", pausa);
+        }
+
+        if (pausa)
+        {
+            Time.timeScale = 0f;
+        }
+
+        else { Time.timeScale = 1f; }
+
+	}
+
+    public void PulsoPaly()
+    {
+        Time.timeScale = 1f;
+        pausa = !pausa;
+        anim.SetBool("activar", !pausa);
+        SceneManager.LoadScene("Escena1");
+        
+    }
+    */
+}
--- EjerciciosUnity/Assets/Scripts/PlayerController.cs
+++ EjerciciosUnity/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
@@ -14,13 +15,13 @@ public class PlayerController : MonoBehaviour {
     Vector2 posicionClick;
 
     //Cosas para atraer los imanes
-    Rigidbody2D rbIman;
+
     Transform trIman;
+    public List<GameObject> imanes;
 
+    //Cosas para la UIIIIIII
 
-    bool dentro;
-    
-    float radio = 5f;
+    public Text textoImanes;
 
 	// Use this for initialization
 	void Start () {
@@ -28,16 +29,23 @@ public class PlayerController : MonoBehaviour {
         player = GetComponent<Rigidbody2D>();
 
         posicionPlayer = GetComponent<Transform>();
+
+        textoImanes.text = imanes.Count.ToString();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        //mueveTeclas();
+        MueveTeclas();
 
-        //sigueRaton();
+        //SigueRaton();
 
-        sigueClicks();
+        //SigueClicks();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Disparar();
+        }
 
     }
 
@@ -47,26 +55,31 @@ public class PlayerController : MonoBehaviour {
     {
         if (col.gameObject.tag == "magnetico")
         {
-            rbIman = col.gameObject.GetComponent<Rigidbody2D>();
+            Rigidbody2D rbIman = col.gameObject.GetComponent<Rigidbody2D>();
             rbIman.simulated = false;
 
             trIman = col.gameObject.GetComponent<Transform>();
             trIman.parent = transform;
 
+            
+            GameObject nuevoIman = col.gameObject;
+            imanes.Add(nuevoIman);
 
+            textoImanes.text = imanes.Count.ToString();
+            
         }
     }
 
 
 
-    void mueveTeclas()
+    void MueveTeclas()
     {
         float h = Input.GetAxis("Horizontal");
         player.velocity = new Vector2(h * fuerza, player.velocity.y);
     }
 
 
-    void sigueRaton()
+    void SigueRaton()
     {
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.y = 0;
@@ -74,7 +87,7 @@ public class PlayerController : MonoBehaviour {
     }
 
 
-    void sigueClicks()
+    void SigueClicks()
     {
         if (Input.GetMouseButton(0))
         {
@@ -87,6 +100,19 @@ public class PlayerController : MonoBehaviour {
 
     }
 
+    void Disparar()
+    {
+        if (imanes.Count > 0)
+        {
+            Rigidbody2D rbImanes = imanes[imanes.Count - 1].GetComponent<Rigidbody2D>();
+            imanes[imanes.Count - 1].transform.parent = null;
+            rbImanes.simulated = true;
+            rbImanes.velocity = Vector2.right * fuerza;
+            imanes.Remove(imanes[imanes.Count - 1]);
+
+            textoImanes.text = imanes.Count.ToString();
+        }
+    }
 
 }
 
